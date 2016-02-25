5b9e8515a4ddb5996e3f507a00fdc68b5c9d8f75
Adam <miknevinas@gmail.com>
2016-02-25 16:15:48 -0700

    multiple bars, styling, legend

--- d3.js
+++ d3.js
@@ -3,103 +3,140 @@ showChart();
 function showChart() {
 
     var barData = [{
-        'x': 'DM1',
-        'y': 5
+        'x': 'DevMountain',
+        'y': 300,
+        'z': 900,
     }, {
-        'x': 'DM2',
-        'y': 20
+        'x': 'Utah',
+        'y': 1200,
+        'z': 900,
     },  {
-        'x': 'DM3',
-        'y': 50
+        'x': 'New York',
+        'y': 1850,
+        'z': 1000,
     }, {
-        'x': 'DM4',
-        'y': 40
-    }, {
-        'x': 'DM5',
-        'y': 70
-    }, {
-        'x': 'DM6',
-        'y': 5
-    }, {
-        'x': 'DM7',
-        'y': 60
-    }, {
-        'x': 'DM8',
-        'y': 90
+        'x': 'San Francisco',
+        'y': 2500,
+        'z': 1200,
     }];
 
+    //Chart Parameters
     var chart = d3.select('#chart'),
         margins = {
             top: 20,
-            right: 20,
+            right: 0,
             bottom: 20,
-            left: 50
+            left: 80
         },
         width = 1000 - margins.left - margins.right,
         height = 500 - margins.top - margins.bottom,
-        xRange = d3.scale.ordinal().rangeRoundBands([margins.left, width - margins.right], 0.1).domain(barData.map(function (d) {
+        xRange = d3.scale.ordinal().rangeRoundBands([margins.left, width - margins.right], 0.1).domain(barData.map(function(d) {
             return d.x;
         })),
 
-        yRange = d3.scale.linear().range([height - margins.top, margins.bottom]).domain([0,100]),
+        yRange = d3.scale.linear().range([height - margins.top, margins.bottom]).domain([0,3000]),
 
         xAxis = d3.svg.axis()
-            .scale(xRange)
-            .tickSize(5)
-            .tickSubdivide(true),
+    .scale(xRange)
+    .tickSize(5)
+    .tickSubdivide(true),
 
         yAxis = d3.svg.axis()
-            .scale(yRange)
-            .tickSize(5)
-            .orient("left")
-            .tickSubdivide(true);
+    .scale(yRange)
+    .tickSize(5)
+    .orient("left")
+    .tickSubdivide(true);
 
-
+    //Set x-axis
     chart.append('svg:g')
         .attr('class', 'x axis')
         .attr('transform', 'translate(0,' + (height - margins.bottom) + ')')
         .call(xAxis)
+        .attr('fill', '#ffffff')
         .append("text")
         .attr("x", width / 2)
-        .attr("y", height - 420)
-        .attr("dx", ".71em")
+        .attr("y", height - 400)
         .style("text-anchor", "middle")
-        .text("Cohort");
+        .text("Location")
+        .attr('fill', '#ffffff')
+        .style("font-size","34px")
+        .style("font-family","Josefin Slab");
 
+    //Set y-axis
     chart.append('svg:g')
         .attr('class', 'y axis')
         .attr('transform', 'translate(' + (margins.left) + ',0)')
         .call(yAxis)
+        .attr('fill', '#ffffff')
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", -45)
+        .attr("y", -77)
         .attr("x",0 - (height / 2))
         .attr("dy", ".71em")
         .style("text-anchor", "middle")
-        .text("Employment Rate %");
+        .text("Cost in $")
+        .attr('fill', '#ffffff')
+        .style("font-size","34px")
+        .style("font-family","Josefin Slab");
 
+    //Show Housing Bars
     chart.selectAll('rect')
         .data(barData)
         .enter()
         .append('rect')
-        .attr('x', function (d) {
-        return xRange(d.x);
-    })
-        .attr('y', function (d) {
-        return yRange(d.y);
-    })
-        .attr('width', xRange.rangeBand())
-        .attr('height', function (d) {
-        return ((height - margins.bottom) - yRange(d.y));
-    })
-        .attr('fill', 'grey')
-        .on('mouseover',function(d){
-        d3.select(this)
-            .attr('fill','orange');
-    })
-        .on('mouseout',function(d){
-        d3.select(this)
-            .attr('fill','grey');
-    });
+        .attr('height', 0)
+        .attr('y', yRange(1))
+        .transition().duration(2000).delay(function(d, i) { return i * 300; })
+        .attr('x', function(d) {return xRange(d.x) + xRange.rangeBand() / 2; })
+        .attr('y', function(d) {return yRange(d.y); })
+        .attr('width', xRange.rangeBand() / 2)
+        .attr('height', function (d) {return ((height - margins.bottom) - yRange(d.y));})
+        .attr('fill', '#C1CED4')
 
+    //Show Expenses Bars
+    chart.selectAll('rect2')
+        .data(barData)
+        .enter()
+        .append('rect')
+        .attr('height', 0)
+        .attr('y', yRange(1))
+        .transition().duration(2000).delay(function(d, i) { return i * 300; })
+        .attr('x', function(d) {return xRange(d.x);})
+        .attr('y', function(d) {return yRange(d.z);})
+        .attr('width', xRange.rangeBand() / 2)
+        .attr('height', function (d) {return ((height - margins.bottom) - yRange(d.z));})
+        .attr('fill', '#05A8E6');
+
+    //Legend
+    var colors = ['#05A8E6', '#C1CED4'];
+    var legendData= ['Expenses', 'Housing'];
+
+    var legend = chart.append("g")
+    .attr("class", "legend")
+    .attr("height", 100)
+    .attr("width", 100)
+    .attr('transform', 'translate(-20,30)');
+
+    var legendRect = legend.selectAll('rect').data(colors);
+
+    legendRect.enter()
+        .append("rect")
+        .attr("x", width - 65)
+        .attr("width", 10)
+        .attr("height", 10);
+
+    legendRect
+        .attr("y", function(d, i) {return i * 20;})
+        .style("fill", function(d) {return d;});
+
+    var legendText = legend.selectAll('text').data(legendData);
+
+    legendText.enter()
+        .append("text")
+        .attr("x", width - 45)
+        .attr('fill', '#ffffff');
+
+    legendText
+        .attr("y", function(d, i) {return i * 20 + 9;})
+        .text(function(d) {return d;}); 
 }
